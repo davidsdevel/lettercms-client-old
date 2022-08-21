fa39f400e8bbdcb1b223f0c8f651c5fbd3322f60
davidsdevel <djgm1206@gmail.com>
2022-08-21 14:38:31 -0400

    Changed sentry DSN

--- config/env.js
+++ config/env.js
@@ -3,7 +3,7 @@ module.exports = {
   FACEBOOK_APP_ID: '549520006389631',
   LETTER_ACCESS_TOKEN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc0FkbWluIjp0cnVlLCJpYXQiOjE2MTY4MjU1MDN9.BlS_E05w8AUhQvsVH0A_T28QC3l3nwqM3e2hP4Qa1RA',
   SENTRY_AUTH_TOKEN: '04eb3000f558442ebb370f96e48d35ee2e1dc0a589a94ea0b2ec5f94e6afe484',
-  SENTRY_DSN: 'https://12cd4d0b82cf48a199c7739451dc2742@o1179001.ingest.sentry.io/6291299',
+  SENTRY_DSN: 'https://8f4ebc8c98ba42b78757dab8eb6a00b5@o1340420.ingest.sentry.io/6613142',
   SENTRY_ORG: 'lettercms',
   SENTRY_PROJECT: 'lettercms-dashboard',
   JWT_AUTH: 'davidsdevel',
